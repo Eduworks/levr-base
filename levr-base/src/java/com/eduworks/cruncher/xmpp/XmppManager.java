0b3993c088bda82e6a897dfba9c69530ccf120e6
Ronald Ray <fritz.ray@eduworks.com>
2014-09-22 11:23:31 -0700

    Attempting to make connection resilient to disconnects.

--- levr-base/src/java/com/eduworks/cruncher/xmpp/XmppManager.java
+++ levr-base/src/java/com/eduworks/cruncher/xmpp/XmppManager.java
@@ -5,20 +5,23 @@ import java.util.Map;
 
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.ConnectionCreationListener;
+import org.jivesoftware.smack.ConnectionListener;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smackx.filetransfer.FileTransferManager;
 
 public class XmppManager
 {
-	static Map<String, XMPPConnection>		connections	= new HashMap<String, XMPPConnection>();
-	static Map<String, FileTransferManager>	ftms		= new HashMap<String, FileTransferManager>();
-	static Map<String, Chat>				chats		= new HashMap<String, Chat>();
+	static Map<String, XMPPConnection> connections = new HashMap<String, XMPPConnection>();
+	static Map<String, FileTransferManager> ftms = new HashMap<String, FileTransferManager>();
+	static Map<String, Chat> chats = new HashMap<String, Chat>();
+	static Map<String,ConnectionListener> cls = new HashMap<String,ConnectionListener>();
 
-	public static XMPPConnection get(String server, String port, String loginHostname, String username, String password)
+	public static XMPPConnection get(String server, String port, String loginHostname, final String username, final String password)
 	{
 		String hash = server + port + username + password;
-		XMPPConnection connection = null;
+		final XMPPConnection connection;
 		if (connections.containsKey(hash))
 			connection = connections.get(hash);
 		else
@@ -35,6 +38,60 @@ public class XmppManager
 				connection.connect();
 			if (!connection.isAuthenticated())
 				connection.login(username, password);
+			if (cls.containsKey(hash))
+				connection.removeConnectionListener(cls.get(hash));
+			ConnectionListener connectionListener = new ConnectionListener()
+			{
+				@Override
+				public void reconnectionSuccessful()
+				{
+				}
+
+				@Override
+				public void reconnectionFailed(Exception arg0)
+				{
+					try
+					{
+						if (!connection.isConnected())
+							connection.connect();
+						if (!connection.isAuthenticated())
+							connection.login(username, password);
+					}
+					catch (XMPPException e)
+					{
+						e.printStackTrace();
+					}
+				}
+
+				@Override
+				public void reconnectingIn(int arg0)
+				{
+				}
+
+				@Override
+				public void connectionClosedOnError(Exception arg0)
+				{
+					arg0.printStackTrace();
+				}
+
+				@Override
+				public void connectionClosed()
+				{
+					try
+					{
+						if (!connection.isConnected())
+							connection.connect();
+						if (!connection.isAuthenticated())
+							connection.login(username, password);
+					}
+					catch (XMPPException e)
+					{
+						e.printStackTrace();
+					}
+				}
+			};
+			cls.put(hash, connectionListener);
+			connection.addConnectionListener(connectionListener);
 		}
 		catch (XMPPException e)
 		{
@@ -45,8 +102,7 @@ public class XmppManager
 		return connection;
 	}
 
-	public static FileTransferManager getFtm(String server, String port, String loginHostname, String username,
-			String password)
+	public static FileTransferManager getFtm(String server, String port, String loginHostname, String username, String password)
 	{
 		String hash = server + port + username + password;
 		XMPPConnection connection = get(server, port, loginHostname, username, password);
