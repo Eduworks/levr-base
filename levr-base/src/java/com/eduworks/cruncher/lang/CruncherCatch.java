ed0c279008375d31de6d53138e0df8e74ec895d5
devlinjunker <devlin.junker@gmail.com>
2014-10-08 11:44:53 -0700

    Added Catch Cruncher

---
+++ levr-base/src/java/com/eduworks/cruncher/lang/CruncherCatch.java
@@ -0,0 +1,80 @@
+package com.eduworks.cruncher.lang;
+
+import java.io.InputStream;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.eduworks.lang.EwMap;
+import com.eduworks.resolver.Cruncher;
+
+public class CruncherCatch extends Cruncher
+{
+
+	@Override
+	public Object resolve(Map<String, String[]> parameters, Map<String, InputStream> dataStreams) throws JSONException
+	{		
+		Object result;
+		try{	
+			result = resolveAChild("try", parameters, dataStreams);
+		}catch(NullPointerException e){
+			if(has("null")){
+				final EwMap<String, String[]> newParams = new EwMap<String, String[]>(parameters);
+				newParams.put("message", new String[] { e.getMessage() });
+				
+				result = resolveAChild("null", newParams, dataStreams);
+			}else{
+				throw e;
+			}
+		}catch(RuntimeException e){
+			if(has("runtime")){
+				final EwMap<String, String[]> newParams = new EwMap<String, String[]>(parameters);
+				newParams.put("message", new String[] { e.getMessage() });
+				
+				result = resolveAChild("runtime", newParams, dataStreams);
+			}else{
+				throw e;
+			}
+			
+		}catch(JSONException e){
+			if(has("any")){
+				final EwMap<String, String[]> newParams = new EwMap<String, String[]>(parameters);
+				newParams.put("message", new String[] { e.getMessage() });
+				
+				result = resolveAChild("any", newParams, dataStreams);
+			}else{
+				throw e;
+			}
+		}finally{
+			if(has("finally")){
+				result = resolveAChild("finally", parameters, dataStreams);
+			}
+		}
+		return result;
+	}
+
+	@Override
+	public String getDescription()
+	{
+		return "Runs the operation identified by 'try' and catches an Exception if catch ('null', 'runtime' or 'any' params) clause is specified, can also run a finally clause (result of finally is returned)";
+	}
+
+	@Override
+	public String getReturn()
+	{
+		return "Object";
+	}
+
+	@Override
+	public String getAttribution()
+	{
+		return ATTRIB_NONE;
+	}
+
+	@Override
+	public JSONObject getParameters() throws JSONException
+	{
+		return jo("try","Resolvable","runtime","Resolvable","any","Resolvable", "finally", "Resolvable");
+	}
+}
