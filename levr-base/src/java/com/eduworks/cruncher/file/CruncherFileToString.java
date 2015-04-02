a084edfa5bff1a649daa2bc238a5c64789d9f8a4
devlinjunker <devlin.junker@gmail.com>
2015-04-02 10:36:11 -0700

    Added Ability for File To String to deal with an ewlist of files.

--- levr-base/src/java/com/eduworks/cruncher/file/CruncherFileToString.java
+++ levr-base/src/java/com/eduworks/cruncher/file/CruncherFileToString.java
@@ -8,6 +8,8 @@ import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.eduworks.lang.EwList;
+import com.eduworks.lang.json.impl.EwJsonArray;
 import com.eduworks.resolver.Context;
 import com.eduworks.resolver.Cruncher;
 import com.eduworks.util.io.InMemoryFile;
@@ -18,14 +20,33 @@ public class CruncherFileToString extends Cruncher
 	@Override
 	public Object resolve(Context c, Map<String, String[]> parameters, Map<String, InputStream> dataStreams) throws JSONException
 	{
-		try
-		{
-			InMemoryFile obj = (InMemoryFile) getObj(c, parameters, dataStreams);
-			return IOUtils.toString(obj.data);
+		Object obj =  getObj(c, parameters, dataStreams);
+		
+		if(obj instanceof InMemoryFile){
+			InMemoryFile file = (InMemoryFile) obj;
+			try
+			{
+				return IOUtils.toString(file.data);
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
 		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
+		else if(obj instanceof EwList<?>){
+			EwList<InMemoryFile> fileList = (EwList<InMemoryFile>) obj;
+			
+			EwJsonArray array = new EwJsonArray();
+			
+			try {
+				for(InMemoryFile file : fileList){
+					array.put(IOUtils.toString(file.data));
+				}
+				
+				return array;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		return null;
 	}
