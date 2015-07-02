129697b1104543f481006c1bd86a069997379cd8
whistlinjoe <rojkell@hotmail.com>
2015-07-02 13:12:51 -0500

    T. Buskirk: Added CruncherSqlUpdate and modified CruncherSql so that the two could share a new base type (SqlTypeCruncher).  SqlTypeCruncher holds connection creation and close logic that is shared between the two.

--- levr-base/src/java/com/eduworks/cruncher/db/sql/CruncherSql.java
+++ levr-base/src/java/com/eduworks/cruncher/db/sql/CruncherSql.java
@@ -4,11 +4,6 @@ import java.io.InputStream;
 import java.io.Reader;
 import java.io.StringWriter;
 import java.sql.Clob;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
@@ -17,129 +12,76 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.eduworks.resolver.Context;
-import com.eduworks.resolver.ContextEvent;
-import com.eduworks.resolver.Cruncher;
 
-public class CruncherSql extends Cruncher
-{
+public class CruncherSql extends SqlTypeCruncher {
+   
+   private JSONArray dataSet = new JSONArray();
 
 	@Override
-	public Object resolve(Context c, Map<String, String[]> parameters, Map<String, InputStream> dataStreams) throws JSONException
-	{
-		String DB_URL = getAsString("sqlConnectionString", c, parameters, dataStreams);
-		String USER = getAsString("sqlUsername", c, parameters, dataStreams);
-		String PASS = getAsString("sqlPassword", c, parameters, dataStreams);
-		boolean mysql = optAsBoolean("sqlMysql", false, c, parameters, dataStreams);
-		boolean sqlserver = optAsBoolean("sqlSqlServer", false, c, parameters, dataStreams);
-		boolean jtds = optAsBoolean("sqlJtds", false, c, parameters, dataStreams);
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		JSONArray ja = new JSONArray();
-		try
-		{
-			// STEP 2: Register JDBC driver
-			if (mysql)
-				Class.forName("com.mysql.jdbc.Driver");
-			if (sqlserver)
-				Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			if (jtds)
-				Class.forName("net.sourceforge.jtds.jdbc.Driver");
-
-			// STEP 3: Open a connection
-			conn = (Connection) c.get(DB_URL);
-			if (conn == null)
-			{
-				conn = DriverManager.getConnection(DB_URL, USER, PASS);
-				final Connection conFinal = conn;
-				c.onFinally(new ContextEvent()
-				{
-					@Override
-					public void go()
-					{
-						if (conFinal != null)
-							try
-							{
-								conFinal.close();
-							}
-							catch (SQLException e)
-							{
-								e.printStackTrace();
-							}
-					}
-				});
-			}
-
-			// STEP 4: Execute a query
-			stmt = conn.createStatement();
-			String sql;
-			sql = getObj(c, parameters, dataStreams).toString();
-			rs = stmt.executeQuery(sql);
-
-			// STEP 5: Extract data from result set
-			while (rs.next())
-			{
-				JSONObject jo = new JSONObject();
-				for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++)
-				{
-					String colName = rs.getMetaData().getColumnName(i);
-					int colType = rs.getMetaData().getColumnType(i);
-					if (colType == java.sql.Types.CHAR)
-						jo.put(colName, rs.getString(colName).trim());
-					else if (colType == java.sql.Types.VARCHAR)
-						jo.put(colName, rs.getString(colName).trim());
-					else if (colType == java.sql.Types.INTEGER)
-						jo.put(colName, rs.getInt(colName));
-					else if (colType == java.sql.Types.DECIMAL)
-						jo.put(colName, rs.getBigDecimal(colName));
-					else if (colType == java.sql.Types.NUMERIC)
-						jo.put(colName, rs.getDouble(colName));
-					else if (colType == java.sql.Types.CLOB)
-					   jo.put(colName, clobToString(rs.getClob(colName)));
-				}
-				ja.put(jo);
-			}
-			// STEP 6: Clean-up environment
-			rs.close();
-			rs = null;
-			stmt.close();
-			stmt = null;
-
-		}
-		catch (SQLException se)
-		{
-			// Handle errors for JDBC
-			se.printStackTrace();
-		}
-		catch (Exception e)
-		{
-			// Handle errors for Class.forName
-			e.printStackTrace();
-		}
-		finally
-		{
-			// finally block used to close resources
-			try
-			{
-				if (rs != null)
-					rs.close();
-			}
-			catch (SQLException se2)
-			{
-			}// nothing we can do
-				// finally block used to close resources
-			try
-			{
-				if (stmt != null)
-					stmt.close();
-			}
-			catch (SQLException se2)
-			{
-			}// nothing we can do
-		}// end try
-		return ja;
+	public Object resolve(Context c, Map<String, String[]> parameters, Map<String, InputStream> dataStreams) throws JSONException {
+	   try {
+	      if (!loadParams(c, parameters, dataStreams)) return null;
+	      openDbConnection(c);
+	      executeSqlStatement();
+	      parseResultSet();
+	      return dataSet;
+	   }
+	   catch (Exception e) {
+	      e.printStackTrace();
+	      return null;
+	   }
+	   finally {
+	      closeAndClean();
+	   }
 	}
-
+	
+	private void executeSqlStatement() throws Exception {
+	   try {
+   	   setStmt(getConn().createStatement());
+   	   setRs(getStmt().executeQuery(getSqlStatement()));
+   	   while(getRs().next()) {
+   	      dataSet.put(parseCurrentResultSetRow());	      
+   	   }
+	   }
+	   catch (Exception e) {
+	      log.debug("Exception executing sql statement:" + getSqlStatement());
+         throw e;
+	   }
+	}
+	
+	private void parseResultSet() throws Exception {
+	   try {
+         while(getRs().next()) {
+            dataSet.put(parseCurrentResultSetRow());        
+         }
+      }
+      catch (Exception e) {
+         log.debug("Exception parsing result set:" + getSqlStatement());
+         throw e;
+      }
+	}
+	
+	private JSONObject parseCurrentResultSetRow() throws Exception {
+	   JSONObject dataRow = new JSONObject();
+	   for (int i = 1; i <= getRs().getMetaData().getColumnCount(); i++) {
+         String colName = getRs().getMetaData().getColumnName(i);
+         int colType = getRs().getMetaData().getColumnType(i);
+         if (colType == java.sql.Types.CHAR)
+            dataRow.put(colName, getRs().getString(colName).trim());
+         else if (colType == java.sql.Types.VARCHAR)
+            dataRow.put(colName, getRs().getString(colName).trim());
+         else if (colType == java.sql.Types.INTEGER)
+            dataRow.put(colName, getRs().getInt(colName));
+         else if (colType == java.sql.Types.DECIMAL)
+            dataRow.put(colName, getRs().getBigDecimal(colName));
+         else if (colType == java.sql.Types.NUMERIC)
+            dataRow.put(colName, getRs().getDouble(colName));
+         else if (colType == java.sql.Types.CLOB)
+            dataRow.put(colName, clobToString(getRs().getClob(colName)));
+      }
+	   return dataRow;	   
+	}
+	
 	private String clobToString(Clob c) throws Exception {
 	   Reader in = c.getCharacterStream();
 	   StringWriter w = new StringWriter();
@@ -148,31 +90,28 @@ public class CruncherSql extends Cruncher
 	}
 	
 	@Override
-	public String getDescription()
-	{
-		// TODO Auto-generated method stub
-		return null;
+   public String[] getResolverNames() {
+      return new String[]{getResolverName(),"sqlQuery"};
+   }
+	
+	@Override
+	public String getDescription() {
+	   return "Executes the given sql statment (obj) and returns the result set as a JSON array of JSON objects.";
 	}
 
 	@Override
-	public String getReturn()
-	{
-		// TODO Auto-generated method stub
-		return null;
+	public String getReturn() {
+	   return "JSONArray";
 	}
 
 	@Override
-	public String getAttribution()
-	{
-		// TODO Auto-generated method stub
-		return null;
+	public String getAttribution() {
+	   return ATTRIB_NONE;
 	}
-
+	
 	@Override
-	public JSONObject getParameters() throws JSONException
-	{
-		// TODO Auto-generated method stub
-		return null;
+	public JSONObject getParameters() throws JSONException {
+	   return jo("obj","String","sqlConnectionString","String","sqlUsername","String","sqlPassword","String","sqlMysql","boolean","sqlSqlServer","boolean","sqlJtds","boolean");
 	}
 
 }
