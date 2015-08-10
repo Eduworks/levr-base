1e18ebc8ff3ac6b36e711b3585f785d04afd54bf
winonadeb <winonadeb@users.noreply.github.com>
2015-08-10 15:42:50 -0500

    reducing to a single allowTags call in CruncherStripTags

--- levr-base/src/java/com/eduworks/cruncher/parse/CruncherStripTags.java
+++ levr-base/src/java/com/eduworks/cruncher/parse/CruncherStripTags.java
@@ -19,13 +19,16 @@ public class CruncherStripTags extends Cruncher
 	public Object resolve(Context c, Map<String, String[]> parameters, Map<String, InputStream> dataStreams) throws JSONException
 	{
 		String text = (String)getObj(c, parameters, dataStreams);
-		Whitelist wl = Whitelist.simpleText().addTags("sub", "sup");
+		Whitelist wl = Whitelist.simpleText();
 		
 		JSONArray allowTags = getAsJsonArray("allowTags", c, parameters, dataStreams);
-		String[] tagList = new String[allowTags.length()];
+		String[] tagList = new String[allowTags.length()+2];
+		tagList[0] = "sub";
+		tagList[1] = "sup";
+		
 		if (allowTags != null) {
 			for (int i=0; i<allowTags.length(); i++) {
-				tagList[i] = allowTags.getString(i);
+				tagList[i+2] = allowTags.getString(i);
 			}
 			wl = wl.addTags(tagList);
 		}
