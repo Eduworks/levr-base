d33ca7550001c9f0ce9e1b65c4816e8ca64cc939
Ronald Ray <fritz.ray@eduworks.com>
2016-01-28 18:58:05 -0800

    Crunchers are stateless, therefore must not use class variables.

--- levr-base/src/java/com/eduworks/cruncher/db/sql/CruncherSql.java
+++ levr-base/src/java/com/eduworks/cruncher/db/sql/CruncherSql.java
@@ -4,6 +4,10 @@ import java.io.InputStream;
 import java.io.Reader;
 import java.io.StringWriter;
 import java.sql.Clob;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
@@ -13,107 +17,130 @@ import org.json.JSONObject;
 
 import com.eduworks.resolver.Context;
 
-public class CruncherSql extends SqlTypeCruncher {
-   
-   private JSONArray dataSet = new JSONArray();
+public class CruncherSql extends SqlTypeCruncher
+{
 
 	@Override
-	public Object resolve(Context c, Map<String, String[]> parameters, Map<String, InputStream> dataStreams) throws JSONException {
-	   try {
-	      if (!loadParams(c, parameters, dataStreams)) return null;
-	      openDbConnection(c);
-	      executeSqlStatement();
-	      parseResultSet();
-	      return dataSet;
-	   }
-	   catch (Exception e) {
-	      e.printStackTrace();
-	      return null;
-	   }
-	   finally {
-	      closeAndClean();
-	   }
+	public Object resolve(Context c, Map<String, String[]> parameters, Map<String, InputStream> dataStreams) throws JSONException
+	{
+		Statement stmt = null;
+		ResultSet rs = null;
+		JSONArray dataSet = new JSONArray();
+		try
+		{
+			Connection conn = openDbConnection(c, parameters, dataStreams);
+			String statement = getObj(c, parameters, dataStreams).toString();
+			try
+			{
+				stmt = conn.createStatement();
+				rs = stmt.executeQuery(statement);
+				while (rs.next())
+				{
+					dataSet.put(parseCurrentResultSetRow(rs));
+				}
+			}
+			catch (Exception e)
+			{
+				log.debug("Exception executing sql statement:" + statement);
+				e.printStackTrace();
+				throw e;
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+		finally
+		{
+			if (rs != null)
+			{
+				try
+				{
+					rs.close();
+				}
+				catch (SQLException e)
+				{
+					e.printStackTrace();
+				}
+				rs = null;
+			}
+			if (stmt != null)
+			{
+				try
+				{
+					stmt.close();
+				}
+				catch (SQLException e)
+				{
+					e.printStackTrace();
+				}
+				stmt = null;
+			}
+		}
+		return dataSet;
 	}
-	
-	private void executeSqlStatement() throws Exception {
-	   try {
-   	   setStmt(getConn().createStatement());
-   	   setRs(getStmt().executeQuery(getSqlStatement()));
-   	   while(getRs().next()) {
-   	      dataSet.put(parseCurrentResultSetRow());	      
-   	   }
-	   }
-	   catch (Exception e) {
-	      log.debug("Exception executing sql statement:" + getSqlStatement());
-	      e.printStackTrace();
-         throw e;
-	   }
+
+	private JSONObject parseCurrentResultSetRow(ResultSet rs) throws Exception
+	{
+		JSONObject dataRow = new JSONObject();
+		for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++)
+		{
+			String colName = rs.getMetaData().getColumnName(i);
+			int colType = rs.getMetaData().getColumnType(i);
+			if (colType == java.sql.Types.CHAR)
+				dataRow.put(colName, rs.getString(colName).trim());
+			else if (colType == java.sql.Types.VARCHAR)
+				dataRow.put(colName, rs.getString(colName).trim());
+			else if (colType == java.sql.Types.INTEGER)
+				dataRow.put(colName, rs.getInt(colName));
+			else if (colType == java.sql.Types.DECIMAL)
+				dataRow.put(colName, rs.getBigDecimal(colName));
+			else if (colType == java.sql.Types.NUMERIC)
+				dataRow.put(colName, rs.getDouble(colName));
+			else if (colType == java.sql.Types.CLOB)
+				dataRow.put(colName, clobToString(rs.getClob(colName)));
+		}
+		return dataRow;
 	}
-	
-	private void parseResultSet() throws Exception {
-	   try {
-         while(getRs().next()) {
-            dataSet.put(parseCurrentResultSetRow());        
-         }
-      }
-      catch (Exception e) {
-         log.debug("Exception parsing result set:" + getSqlStatement());
-	      e.printStackTrace();
-         throw e;
-      }
-	}
-	
-	private JSONObject parseCurrentResultSetRow() throws Exception {
-	   JSONObject dataRow = new JSONObject();
-	   for (int i = 1; i <= getRs().getMetaData().getColumnCount(); i++) {
-         String colName = getRs().getMetaData().getColumnName(i);
-         int colType = getRs().getMetaData().getColumnType(i);
-         if (colType == java.sql.Types.CHAR)
-            dataRow.put(colName, getRs().getString(colName).trim());
-         else if (colType == java.sql.Types.VARCHAR)
-            dataRow.put(colName, getRs().getString(colName).trim());
-         else if (colType == java.sql.Types.INTEGER)
-            dataRow.put(colName, getRs().getInt(colName));
-         else if (colType == java.sql.Types.DECIMAL)
-            dataRow.put(colName, getRs().getBigDecimal(colName));
-         else if (colType == java.sql.Types.NUMERIC)
-            dataRow.put(colName, getRs().getDouble(colName));
-         else if (colType == java.sql.Types.CLOB)
-            dataRow.put(colName, clobToString(getRs().getClob(colName)));
-      }
-	   return dataRow;	   
-	}
-	
-	private String clobToString(Clob c) throws Exception {
-	   Reader in = c.getCharacterStream();
-	   StringWriter w = new StringWriter();
-	   IOUtils.copy(in, w);
-	   return w.toString();
-	}
-	
-	@Override
-   public String[] getResolverNames() {
-      return new String[]{getResolverName(),"sqlQuery"};
-   }
-	
-	@Override
-	public String getDescription() {
-	   return "Executes the given sql statment (obj) and returns the result set as a JSON array of JSON objects.";
+
+	private String clobToString(Clob c) throws Exception
+	{
+		Reader in = c.getCharacterStream();
+		StringWriter w = new StringWriter();
+		IOUtils.copy(in, w);
+		return w.toString();
 	}
 
 	@Override
-	public String getReturn() {
-	   return "JSONArray";
+	public String[] getResolverNames()
+	{
+		return new String[] { getResolverName(), "sqlQuery" };
 	}
 
 	@Override
-	public String getAttribution() {
-	   return ATTRIB_NONE;
+	public String getDescription()
+	{
+		return "Executes the given sql statment (obj) and returns the result set as a JSON array of JSON objects.";
 	}
-	
+
 	@Override
-	public JSONObject getParameters() throws JSONException {
-	   return jo("obj","String","sqlConnectionString","String","sqlUsername","String","sqlPassword","String","sqlMysql","boolean","sqlSqlServer","boolean","sqlJtds","boolean");
+	public String getReturn()
+	{
+		return "JSONArray";
+	}
+
+	@Override
+	public String getAttribution()
+	{
+		return ATTRIB_NONE;
+	}
+
+	@Override
+	public JSONObject getParameters() throws JSONException
+	{
+		return jo("obj", "String", "sqlConnectionString", "String", "sqlUsername", "String", "sqlPassword", "String", "sqlMysql", "boolean", "sqlSqlServer",
+				"boolean", "sqlJtds", "boolean");
 	}
 
 }
--- levr-base/src/java/com/eduworks/cruncher/db/sql/CruncherSqlUpdate.java
+++ levr-base/src/java/com/eduworks/cruncher/db/sql/CruncherSqlUpdate.java
@@ -1,70 +1,110 @@
 package com.eduworks.cruncher.db.sql;
 
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Map;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.eduworks.resolver.Context;
 
-public class CruncherSqlUpdate extends SqlTypeCruncher {
-   
-   private int numRowsAffected;
+public class CruncherSqlUpdate extends SqlTypeCruncher
+{
 
-   @Override
-   public Object resolve(Context c, Map<String, String[]> parameters, Map<String, InputStream> dataStreams) throws JSONException {
-      try {
-         if (!loadParams(c, parameters, dataStreams)) return null;
-         openDbConnection(c);         
-         executeSqlUpdate();
-         return numRowsAffected;
-      }
-      catch (Exception e) {
-         e.printStackTrace();
-         return -1;
-      }
-      finally {
-         closeAndClean();
-      }
-   }
-   
-   private void executeSqlUpdate() {      
-      try {
-         setStmt(getConn().createStatement());
-         numRowsAffected = getStmt().executeUpdate(getSqlStatement());
-         commitTransaction();
-      }
-      catch (Exception e) {
-         log.debug("Exception executing sql update: " + getSqlStatement());
-	      e.printStackTrace();
-         rollbackTransaction();         
-      }
-   }
-   
-   @Override
-   public String[] getResolverNames() {
-      return new String[]{getResolverName(),"sqlInsert","sqlDelete"};
-   }
+	private int numRowsAffected;
 
-   @Override
-   public String getDescription() {
-      return "Executes the given sql update, insert, or delete (obj) and returns the number of rows affected.  Returns -1 if an error occurred.";
-   }
+	@Override
+	public Object resolve(Context c, Map<String, String[]> parameters, Map<String, InputStream> dataStreams) throws JSONException
+	{
 
-   @Override
-   public String getReturn() {
-      return "Number";
-   }
+		Statement stmt = null;
+		ResultSet rs = null;
+		try
+		{
+			Connection conn = openDbConnection(c, parameters, dataStreams);
+			String statement = getObj(c, parameters, dataStreams).toString();
+			try
+			{
+				stmt = conn.createStatement();
+				numRowsAffected = stmt.executeUpdate(statement);
+				conn.commit();
+				return numRowsAffected;
+			}
+			catch (Exception e)
+			{
+				conn.rollback();
+				e.printStackTrace();
+				return -1;
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+		finally
+		{
+			if (rs != null)
+			{
+				try
+				{
+					rs.close();
+				}
+				catch (SQLException e)
+				{
+					e.printStackTrace();
+				}
+				rs = null;
+			}
+			if (stmt != null)
+			{
+				try
+				{
+					stmt.close();
+				}
+				catch (SQLException e)
+				{
+					e.printStackTrace();
+				}
+				stmt = null;
+			}
+		}
+	}
 
-   @Override
-   public String getAttribution() {
-      return ATTRIB_NONE;
-   }
+	@Override
+	public String[] getResolverNames()
+	{
+		return new String[] { getResolverName(), "sqlInsert", "sqlDelete" };
+	}
 
-   @Override
-   public JSONObject getParameters() throws JSONException {
-      return jo("obj","String","sqlConnectionString","String","sqlUsername","String","sqlPassword","String","sqlMysql","boolean","sqlSqlServer","boolean","sqlJtds","boolean");
-   }
+	@Override
+	public String getDescription()
+	{
+		return "Executes the given sql update, insert, or delete (obj) and returns the number of rows affected.  Returns -1 if an error occurred.";
+	}
+
+	@Override
+	public String getReturn()
+	{
+		return "Number";
+	}
+
+	@Override
+	public String getAttribution()
+	{
+		return ATTRIB_NONE;
+	}
+
+	@Override
+	public JSONObject getParameters() throws JSONException
+	{
+		return jo("obj", "String", "sqlConnectionString", "String", "sqlUsername", "String", "sqlPassword", "String", "sqlMysql", "boolean", "sqlSqlServer",
+				"boolean", "sqlJtds", "boolean");
+	}
 
 }
