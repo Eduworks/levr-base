e3e17b516e0f36b0d5a583ccf4dcb04ec781029f
RobbyTheBelgian <robby.goetschalckx@gmail.com>
2016-04-07 13:31:50 -0700

    Added new optional parameter 'samplesize'. When 'random' and 'samplesize' >=0 a random sample of n distinct randomly sampled elements of the original array will be returned.

--- levr-base/src/java/com/eduworks/cruncher/lang/CruncherGetIndex.java
+++ levr-base/src/java/com/eduworks/cruncher/lang/CruncherGetIndex.java
@@ -1,8 +1,10 @@
 package com.eduworks.cruncher.lang;
 
 import java.io.InputStream;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeSet;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,6 +25,10 @@ public class CruncherGetIndex extends Cruncher
 		Object objx = getObj(c, parameters, dataStreams);
 		Integer index = optAsInteger("index", 0,c, parameters, dataStreams);
 		boolean random = false;
+		Integer sampleSize=optAsInteger("samplesize",-1,c,parameters,dataStreams);
+		
+		//System.out.println("sampleSize: "+sampleSize);
+		
 		if (optAsBoolean("random",false,c,parameters,dataStreams))
 			random = true;
 		if (objx instanceof List)
@@ -30,8 +36,31 @@ public class CruncherGetIndex extends Cruncher
 			List obj = (List) objx;
 			if (obj == null)
 				return null;
-			if (random) 
-				index = EwRandom.r(obj.size());
+			if (random){
+				if (sampleSize >= 0){
+					
+					// If you try to get too many values, you get the full original list instead.
+					if (sampleSize >= obj.size())
+						return obj;
+					TreeSet<Integer> selectedIndecesSoFar=new TreeSet<Integer>();
+					JSONArray selectedSoFar = new JSONArray();
+					for (int i = 0; i < sampleSize; i++){
+						int selectedIndex=EwRandom.r(obj.size()-i);
+						Iterator<Integer> iter = selectedIndecesSoFar.iterator();
+						while (iter.hasNext() && iter.next() <= selectedIndex)
+							selectedIndex++;
+						selectedIndecesSoFar.add(selectedIndex);
+						selectedSoFar.put(obj.get(selectedIndex));
+						System.out.println(selectedSoFar.length());
+					}
+					return selectedSoFar;				
+				
+				}
+				else{
+					return obj.get(EwRandom.r(obj.size()));
+				}
+			}	
+				
 			if (obj.size() > index && index >= 0)
 				return obj.get(index);
 		}
@@ -42,7 +71,30 @@ public class CruncherGetIndex extends Cruncher
 			JSONArray obj = (JSONArray) objx;
 			if (obj == null)
 				return null;
-			if (random) index = EwRandom.r(obj.length());
+			if (random){
+				if (sampleSize >= 0){
+					
+					// If you try to get too many values, you the original list instead.
+					if (sampleSize >= obj.length())
+						return obj;
+					TreeSet<Integer> selectedIndecesSoFar=new TreeSet<Integer>();
+					JSONArray selectedSoFar = new JSONArray();
+					for (int i = 0; i < sampleSize; i++){
+						int selectedIndex=EwRandom.r(obj.length()-i);
+						Iterator<Integer> iter = selectedIndecesSoFar.iterator();
+						while (iter.hasNext() && iter.next() <= selectedIndex)
+							selectedIndex++;
+						selectedIndecesSoFar.add(selectedIndex);
+						selectedSoFar.put(obj.get(selectedIndex));
+					}
+					return selectedSoFar;				
+				
+				}
+				else{
+					return obj.get(EwRandom.r(obj.length()));
+				}
+			}
+			//if (random) index = EwRandom.r(obj.length());
 			if (obj.length() > index && index >= 0)
 				return obj.get(index);
 		}
@@ -52,7 +104,7 @@ public class CruncherGetIndex extends Cruncher
 	@Override
 	public String getDescription()
 	{
-		return "Indexes into an array and retreives the item at the index designated by 'index'.\nSoft will make it not error out if obj is not a JSONArray.\nIf random, a random item from the array is returned.";
+		return "Indexes into an array and retreives the item at the index designated by 'index'.\nSoft will make it not error out if obj is not a JSONArray.\nIf random and samplesize is not given, a random element from the array is returned. If random and samplesize = n, an array containing n distinct random item from the array is returned (or the original list if n is too large).";
 	}
 
 	@Override
@@ -70,6 +122,6 @@ public class CruncherGetIndex extends Cruncher
 	@Override
 	public JSONObject getParameters() throws JSONException
 	{
-		return jo("obj", "JSONArray", "index", "Number", "?soft", "Boolean", "?random", "Boolean");
+		return jo("obj", "JSONArray", "index", "Number", "?soft", "Boolean", "?random", "Boolean", "","?samplesize","Number");
 	}
 }
